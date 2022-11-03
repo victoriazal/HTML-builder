e69631cb3fa233218e2706dbdc57ef915d4020fb
victoriazal <victoriazalomnova19@gmail.com>
2022-11-03 23:24:30 +0300

    feat: solve task number two

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const readline = require('readline')
+const path = require('node:path');
+
+let newFile = fs.createWriteStream('new-text.txt');
+
+process.stdout.write('Hello beautiful person, let write something..? type exit if you wanna go do other things:)\n');
+
+let createNewFile = (userInput) => {
+  if (userInput.toString().trim() === 'exit') {
+    process.stdout.write('K,bye honey\n');
+    process.exit();
+  } 
+  else {
+    newFile.write(`${userInput.toString()}`)
+    process.stdout.write('Great, what else?\n');
+  }
+}
+process.stdin.on('data', createNewFile)
+
+
+
+
+
+
+
