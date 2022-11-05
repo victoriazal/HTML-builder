9d58f165e18c5ae4af42d761635ae99378b7d55a
victoriazal <victoriazalomnova19@gmail.com>
2022-11-06 00:02:20 +0300

    feat: solve task number 4 , litterally cant believe i came this far with node

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const { Console } = require('console')
+const fs = require('fs')
+const path = require('path')
+
+const newFolderPath = path.join(__dirname, "files-copy")
+const oldFolderPath = path.join(__dirname,"files")
+
+
+const createNewFolder = fs.promises.mkdir(newFolderPath,{recursive:true})
+
+createNewFolder.catch(err =>{
+  console.log(err)
+})
+createNewFolder.then( newFolder =>{
+  return(newFolder)
+})
+
+let readOurFolder = fs.promises.readdir(`${oldFolderPath}`,{withFileTypes: true})
+readOurFolder.catch(err =>{
+  console.log(err)
+})
+
+readOurFolder.then(oldFolder =>{
+  oldFolder.forEach(element => {
+    const pathToTakeOldFiles = path.join(__dirname,"files",`${element.name}`)
+    const pathToPutNewOnes = path.join(__dirname,"files-copy",`${element.name}`)
+    fs.copyFile(`${pathToTakeOldFiles}`, `${pathToPutNewOnes}`, err => {
+      if(err) throw err; 
+    });
+  });
+  
+})
 
