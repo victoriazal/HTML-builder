cdc03f0aa11b3111291ae1ac43a5ea67868b4181
victoriazal <victoriazalomnova19@gmail.com>
2022-11-06 12:55:39 +0300

    feat: task number five create a new file and copy everything from the styles files

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+
+const fs = require('fs')
+const path = require('node:path')
+const readline = require('readline');
+
+let myPath = path.join(__dirname, 'styles')
+let pathForBundleFIle = path.join(__dirname , 'bundle.css')
+let newFile = fs.createWriteStream(`${pathForBundleFIle}`);
+
+
+let readingFiles = fs.promises.readdir(`${myPath}`,{withFileTypes: true})
+
+readingFiles.catch(err => {
+  console.log(err)
+})
+readingFiles.then(files =>{
+    files.forEach(value =>{
+      if(path.extname(value.name)==='.css'){
+        let readingPath = path.join(`${myPath}`, `${value.name}`);
+        const myInterface = readline.createInterface({
+          input: fs.createReadStream(`${readingPath}`)
+        });
+        myInterface.on('line', (fileLine) => {
+          newFile.write(`${fileLine}\n`)
+        });
+    }
+  })
+})
